6ee626bd849bafbdd742b2597d6a21fe86661f54
ParthRKhakre <parthrkhakre@gmail.com>
2025-12-26 23:44:39 +0530

    Some files

---
+++ CPP/Array/7-RotateByOnePlace.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+
+    int n;
+    cin>>n;
+
+    int a[n];
+
+    for(int i = 0;i<n;i++) cin>>a[i];
+
+    int temp = a[0];
+    for(int i = 0;i<n;i++){
+        a[i] = a[i+1];
+    }
+    a[n-1] = temp;
+
+    for(auto it:a){
+        cout<<it<<" ";
+    }
+
+}
